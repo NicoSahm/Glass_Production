d0c21d130fb0f5d5ac60f8ebbbe30965b78c43f4
Daniel Knape <danielknape@outlook.com>
2019-10-12 09:52:53 +0200

    Methode

--- silos.py
+++ silos.py
@@ -1,6 +1,19 @@
 class silos(object):
-    pass
+    
+
+    def fillSilos(self, currentState, siloMaxLevel):
+        diff = siloMaxLevel - currentState
+        fullFillupTimeMin = 20
+        fullFillupTimeSec = fullFillupTimeMin * 60
+        fillupPerSec = fullFillupTimeSec / siloMaxLevel
+        predictedFillupTime = diff * fillupPerSec
+        return predictedFillupTime
 
 
+x = silos()
 
-
+print(x.fillSilos(100,2000))
+# 1900 fehlen
+# 1200 sek Gesamtdauer
+# 0,6 pro Sekunde
+# 1140 Sekunden dauert die Befüllung
