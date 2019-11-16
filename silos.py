64c365064cda94ee6ad209ee6ab51d6e62dd5f33
Daniel Knape <danielknape@outlook.com>
2019-11-16 09:54:53 +0100

    Fertig

--- silos.py
+++ silos.py
@@ -4,13 +4,21 @@ class silos(object):
     
     #Methods for: calculating current time, getting the weight of the silos and saving them, fill the silos, open vault (startTime, totalTime)
 
-    sandSiloCurrentLevel = None
-    ncSiloCurrentLevel = None
-    limeSiloCurrentLevel = None
+    #Silo1 = Sand
+    #Silo2
+    #Silo3
+
+    sandSiloCurrentLevel = 2000
+    ncSiloCurrentLevel = 500
+    limeSiloCurrentLevel = 500
 
     sandSiloMaxLevel = 2000
     ncSiloMaxLevel = 500
     limeSiloMaxLevel = 500
+
+    sand_name = "Sand"
+    nc_name = "NC"
+    lime_name = "Kalk"
 
     def getRemainingFillingTime(self, providedSiloName):
         siloMaxLevel = silos.siloMaxLevelSelector(self, providedSiloName)
@@ -38,6 +46,15 @@ class silos(object):
         else:
             return silos.limeSiloCurrentLevel
 
+    def setSiloLevel(self, siloName, amount):
+        if(siloName == silos.sandSiloCurrentLevel):
+            silos.sandSiloCurrentLevel = amount
+        elif(siloname == silos.ncSiloCurrentLevel):
+            silos.ncSiloCurrentLevel = amount
+        else:
+            silos.limeSiloCurrentLevel = amount
+        
+
     def fillSilos(self, providedSiloName):
         siloMaxLevel = silos.siloMaxLevelSelector(self, providedSiloName)
         siloCurrentLevel = silos.siloSelector(self, providedSiloName)
@@ -48,11 +65,24 @@ class silos(object):
                 break
             else:
                 siloCurrentLevel += 0.6
+                silos.setSiloLevel(self, providedSiloName, siloCurrentLevel)
                 time.sleep(1)
 
+    def fillMixer(self, providedSiloName, amount):
+        siloCurrentLevel = silos.siloSelector(self, providedSiloName)
+        if(siloCurrentLevel < amount):
+            print("Error. Not enough material")
+        else:
+            siloCurrentLevel -= amount
+            silos.setSiloLevel(self, providedSiloName, siloCurrentLevel)
+            return amount
 # Method to open the vaults(Total Weight, Starttime)            
-
 
 x = silos()
 
-print(x.getRemainingFillingTime(x.sandSiloCurrentLevel))
+#print(x.getRemainingFillingTime(x.sandSiloCurrentLevel))
+print(x.fillSilos(x.sandSiloCurrentLevel))
+print(x.sandSiloCurrentLevel)
+filling = x.fillMixer(x.sandSiloCurrentLevel, 400)
+print(filling)
+print(x.sandSiloCurrentLevel)
